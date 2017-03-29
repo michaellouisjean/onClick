949a08530981977806a3ebb70f59c258861da076
michaellouisjean <michaellouisjean@gmail.com>
2017-03-29 17:43:44 +0200

    Added pull refresh on Favorites.js

--- src/components/scenes/Favorites.js
+++ src/components/scenes/Favorites.js
@@ -1,11 +1,15 @@
 // import library
 import React from 'react';
 import {
-  View,
-  Text,
-  ListView,
   ActivityIndicator,
+  ListView,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  Text,
   TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import Global from '../core/Global';
@@ -17,10 +21,14 @@ import Config from '../core/Config';
 //const FAVORITES = [];
 // create component & render
 class OffersListScene extends React.Component {
+  static title = '<RefreshControl>';
+  static description = 'Adds pull-to-refresh support to a scrollview.';
   constructor(props) {
     super(props);
     this.user = Api.getUser();
     this.state = {
+      isRefreshing: false,
+      loaded: 0,
       dataSource: new ListView.DataSource({
         rowHasChanged: (r1, r2) => r1 !== r2,
       }),
@@ -37,6 +45,35 @@ class OffersListScene extends React.Component {
         });
       });
   }
+
+  _onRefresh = () => {
+    console.log('#REFRESH : _onRefresh');
+    this.setState({ isRefreshing: true });
+    Api.getFavorites()
+      .then(result => {
+        console.log('#REFRESH : ComponentDidMount =====>>>>>', result);
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(result)
+        });
+      });
+    setTimeout(() => {
+      console.log('#REFRESH : setTimeout()');
+      // prepend 10 items
+      const rowData = Array.from(new Array(10))
+        .map((val, i) => ({
+          text: `Loaded row ${+this.state.loaded + i}`,
+          clicks: 0,
+        }))
+        .concat(this.state.rowData);
+        console.log('REFRESH : isLoaded =====>>>>>', this.state.loaded);
+
+      this.setState({
+        loaded: this.state.loaded + 10,
+        isRefreshing: false,
+        rowData,
+      });
+    }, 500);
+  };
 
   showFavorites() {
     if (this.state.dataSource.getRowCount() === 0) {
@@ -61,44 +98,55 @@ class OffersListScene extends React.Component {
       <ListView
         dataSource={this.state.dataSource}
         renderRow={this.renderCard}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.isRefreshing}
+            onRefresh={this._onRefresh}
+            tintColor={Global.colors.primary}
+            title="Loading..."
+            titleColor="#00ff00"
+            colors={['#ff0000', '#00ff00', '#0000ff']}
+            progressBackgroundColor="#ffff00"
+          />
+        }
       />
     );
   }
 
 
-renderCard(rowData) {
-  console.log('NOW RENDER CARDS !!!!!!!!!');
-  if (this.user.status === 'candidate') {
-    console.log('user is a candidate');
-    return (<OfferCard {...rowData} />);
+  renderCard(rowData) {
+    console.log('NOW RENDER CARDS !!!!!!!!!');
+    if (this.user.status === 'candidate') {
+      console.log('user is a candidate');
+      return (<OfferCard {...rowData} />);
+    }
+    console.log('user is a recruiter');
+    return (<Card {...rowData} />);
   }
-  console.log('user is a recruiter');
-  return (<Card {...rowData} />);
-}
 
-render() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingTop: 64,
-        paddingBottom: 50,
-      }}
-    >
+  render() {
+    return (
       <View
         style={{
           flex: 1,
           justifyContent: 'center',
           alignItems: 'center',
+          paddingTop: 64,
+          paddingBottom: 50,
         }}
       >
-        {this.showFavorites()}
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {this.showFavorites()}
+        </View>
       </View>
-    </View>
-  );
-}
+    );
+  }
 }
 
 // make component avalaible to other parts
