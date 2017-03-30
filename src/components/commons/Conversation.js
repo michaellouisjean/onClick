6864f7fa0a539f6005cfa515c11144c028c44b4c
michaellouisjean <michaellouisjean@gmail.com>
2017-03-30 11:52:26 +0200

    conversations ok

--- src/components/commons/Conversation.js
+++ src/components/commons/Conversation.js
@@ -31,24 +31,47 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   }
 });
-
+const MESSAGES = [
+  "Je peux vous rejoindre directement. Pouvez-vous me donner une adresse ?",
+  "Vous: Bonjour, seriez-vous disponible pour discuter de l'offre d'emploi que je propose ?",
+  "Bonjour, je serais très intéresser pour échanger avec vous à propose de votre offre de stage. Êtes-vous disponible ?",
+  "Vous: Oui tout à fait. Je vous laisse mais disons nous à demain dans ce cas",
+  "Vous: Pourriez-vous commencer la semaine prochaine ?",
+  "Je vous remercie, c'est tout-à-fait le poste que je cherchais",
+  "Vous: Super ! Merci beaucoup. Et bien je vous dis à tout à l'heure dans ce cas",
+  "Je peux, si vous voulez, transmettre votre offre à une amie qui cherche exactement ce type d'emploi",
+  "J'ai des bases solides sur NodeJS React et React Native. Voici mon lien vers Github: ",
+  "Côté backend j'ai surtout utilisé Ruby on Rails Php et MySQL",
+  "Surtout React et React Native pour la partie Front.",
+  "Oui je suis Dev Fullstack Javascript et également Scrum Master",
+];
 // create component & render
 class Conversation extends React.Component {
+  renderMessage() {
+    const number = Math.floor(Math.random() * ((MESSAGES.length - 1) - 0) + 0);
+    return MESSAGES[number];
+  }
+
   render() {
-    //console.log(this.props);
+    console.log(this.props);
     return (
       <TouchableOpacity
         style={styles.container}
-        onPress={() => Actions.tchat()}
+        onPress={() => Actions.tchat({ speakerId: this.props._id })}
       >
         <View style={{ flex: 1 }}>
-          <DefaultAvatar size={40} />
+          <DefaultAvatar
+            size={40}
+            photo={this.props.photo}
+          />
         </View>
 
         <View style={{ flex: 5 }}>
-          <Text style={styles.title}>John Doe</Text>
-          <Text numberOfLines={2}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.</Text>
+          <Text style={styles.title}>{this.props.firstname} {this.props.lastname}</Text>
+          <Text
+            numberOfLines={2}
+            style={{ color: '#515151' }}
+          >{this.renderMessage()}</Text>
         </View>
       </TouchableOpacity>
     );
--- src/components/scenes/Save.js
+++ src/components/scenes/Save.js
@@ -1,106 +1,43 @@
 // import library
 import React from 'react';
 import {
-  RefreshControl,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableWithoutFeedback,
   View,
+  ListView,
+  StyleSheet,
 } from 'react-native';
 
+// import components
+import Conversation from '../commons/Conversation';
+
+// styles
 const styles = StyleSheet.create({
-  row: {
-    borderColor: 'grey',
-    borderWidth: 1,
-    padding: 20,
-    backgroundColor: '#3a5795',
-    margin: 5,
-  },
-  text: {
-    alignSelf: 'center',
-    color: '#fff',
-  },
-  scrollview: {
-    flex: 1,
-  },
+  container: {
+    paddingTop: 65,
+  }
 });
 
-class Row extends React.Component {
-  _onClick = () => {
-    this.props.onClick(this.props.data);
-  };
+// create component & render
+class TchatListScene extends React.Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource: ds.cloneWithRows([1, 1, 1, 1, 1]),
+    };
+  }
 
   render() {
     return (
-      <TouchableWithoutFeedback onPress={this._onClick} >
-        <View style={styles.row}>
-          <Text style={styles.text}>
-            {`${this.props.data.text} (${this.props.data.clicks} clicks)`}
-          </Text>
-        </View>
-      </TouchableWithoutFeedback>
+      <View style={[{ flex: 1 }, styles.container]} >
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={() => <Conversation />}
+        />
+      </View>
     );
   }
 }
 
-export default class Test extends React.Component {
-  static title = '<RefreshControl>';
-  static description = 'Adds pull-to-refresh support to a scrollview.';
-
-  state = {
-    isRefreshing: false,
-    loaded: 0,
-    rowData: Array.from(new Array(20)).map(
-      (val, i) => ({ text: `Initial row ${i}`, clicks: 0 })),
-  };
-
-  _onClick = (row) => {
-    row.clicks++;
-    this.setState({
-      rowData: this.state.rowData,
-    });
-  };
-
-  _onRefresh = () => {
-    this.setState({ isRefreshing: true });
-    setTimeout(() => {
-      // prepend 10 items
-      const rowData = Array.from(new Array(10))
-        .map((val, i) => ({
-          text: `Loaded row ${+this.state.loaded + i}`,
-          clicks: 0,
-        }))
-        .concat(this.state.rowData);
-
-      this.setState({
-        loaded: this.state.loaded + 10,
-        isRefreshing: false,
-        rowData,
-      });
-    }, 5000);
-  };
-
-  render() {
-    const rows = this.state.rowData.map((row, ii) => <Row key={ii} data={row} onClick={this._onClick} />);
-    return (
-      <ScrollView
-        style={styles.scrollview}
-        refreshControl={
-          <RefreshControl
-            refreshing={this.state.isRefreshing}
-            onRefresh={this._onRefresh}
-            tintColor="#ff0000"
-            title="Loading..."
-            titleColor="#00ff00"
-            colors={['#ff0000', '#00ff00', '#0000ff']}
-            progressBackgroundColor="#ffff00"
-          />
-        }
-      >
-        {rows}
-      </ScrollView>
-    );
-  }
-}
+// make component avalaible to other parts
+export default TchatListScene;
 
